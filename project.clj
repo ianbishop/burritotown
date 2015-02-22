c5e6b894ef6dd4ec31e385c88288365a19f44629
Angus Fletcher <angusfletcher@Anguss-MacBook-Air.local>
2015-02-22 18:22:37 -0400

    Add aot

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject yalp "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
+  :profiles {:uberjar {:aot :all}}
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
