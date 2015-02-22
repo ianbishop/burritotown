40fa8e62510e0a598d96538cb86259161ec8bbad
Angus Fletcher <angusfletcher@Anguss-MacBook-Air.local>
2015-02-22 16:17:49 -0400

    ADd more icons

--- resources/js/components.js
+++
@@ -1,6 +0,0 @@
-$("#city-picker #submit").click(function(e) {
-    var city = $("#cities option:selected").text();
-    var path = "/cities/" + city;
-    $.getJSON(path, function(data) {
-        console.log(data)});
-    console.log(path)});
