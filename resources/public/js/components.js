e10a67cb5004f13c1d861dfa7b6d5e6e1aa9cbc9
Angus Fletcher <angusfletcher@Anguss-MacBook-Air.local>
2015-02-22 16:14:43 -0400

    add images

--- resources/public/js/components.js
+++ resources/public/js/components.js
@@ -1,3 +1,19 @@
+function getSimilars(city) {
+    $("#compare-to >").remove();
+    var path = "/city/" + city + "/similar";
+    _.each(["Compare To:", "Vancouver", "Montreal", "Toronto"], function(v) {
+        $("<option>" + v + "</option>").appendTo("#compare-to");
+    });
+    $.getJSON(path, function(data) {
+        var opts = _.each(data.similar, function(v) {
+            $("<option>" + v + "</option>").appendTo("#compare-to");
+        });
+    });
+}
+
+function selectCategory() {
+}
+
 $("#city-picker #submit").click(function(e) {
     var city = $("#cities option:selected").text();
     var path = "/city/" + city;
