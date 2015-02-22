dd8946e2b41dbec44c82d764933d5b6ef30be9b1
Angus Fletcher <angusfletcher@Anguss-MacBook-Air.local>
2015-02-22 18:42:18 -0400

    NUMBER

--- src/yalp/core.clj
+++ src/yalp/core.clj
@@ -156,5 +156,5 @@
                                  (assoc a city data)))
                              {}
                              (dissoc city-data ""))]
-    (run-server (handler city-data) {:port (or port 8080)}))
+    (run-server (handler city-data) {:port (or port (num 8080))}))
   (log/info "Server started"))
